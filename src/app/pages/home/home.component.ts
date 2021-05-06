340cabcb4d43f748d57900a7bd557b5d5b43f21b
kiranitor123 <rolandoalvarezfaye@gmail.com>
2021-05-06 18:14:24 -0400

    Mojix coding challenge

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,15 +3,44 @@ import {
   Component,
   OnChanges,
   SimpleChanges
-} from "@angular/core";
+} from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { ApiService } from '../../api/api.service';
+import { SongResponse } from '../../song/song.model';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnChanges, AfterViewInit {
+  songList: Observable<SongResponse>;
+  term: string = 'null';
+  select: boolean = false;
+  kind: string = 'music';
+
+  constructor(private itunes: ApiService) {
+    this.songList = this.itunes.searchSongs(this.term, this.kind);
+  }
+
+  get isSelect() {
+    return this.select;
+  }
+  set isSelect(value) {
+    this.kind = value ? 'movie' : 'music';
+    this.songList = this.itunes.searchSongs(this.term, this.kind);
+  }
+
   ngOnChanges(changes: SimpleChanges): void {}
 
   ngAfterViewInit() {}
+
+  getItems(ev) {
+    const value = ev.target.value;
+    if (value.trim().length > 0) {
+      this.term = value;
+      console.log(value);
+      this.songList = this.itunes.searchSongs(this.term, this.kind);
+    }
+  }
 }
--- src/app/song/song.component.ts
+++ src/app/song/song.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { SongModel } from "./song.model";
+import { Component, Input, OnInit } from '@angular/core';
+import { SongModel } from './song.model';
 
 @Component({
-  selector: "app-song",
-  templateUrl: "./song.component.html",
-  styleUrls: ["./song.component.scss"]
+  selector: 'app-song',
+  templateUrl: './song.component.html',
+  styleUrls: ['./song.component.scss']
 })
 export class SongComponent implements OnInit {
   @Input() currentSong: SongModel;
-
+  @Input() position: SongModel;
   constructor() {}
 
   ngOnInit(): void {}
